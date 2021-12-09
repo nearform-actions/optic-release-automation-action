e10569d72adc56daf6a172014e7c71ea376314ea
Tomas Monteiro <tomasmonteiro@Tomass-MBP.lan>
2021-12-09 15:03:56 +0000

    fix: error handling in callApi method

--- utils/callApi.js
+++ utils/callApi.js
@@ -1,27 +1,28 @@
 'use strict'
 
 const fetch = require('node-fetch')
-const { logWarning } = require('../log')
+const { logWarning, logError } = require('../log')
 
 const GITHUB_APP_URL = 'https://github.com/apps/optic-release-automation'
 
 // Github does not allow a new workflow run to be triggered as a result of an action using the same `GITHUB_TOKEN`.
 // Hence all write ops are being done via an external GitHub app.
 const callApi = async ({ method, endpoint, body }, inputs) => {
-  const response = await fetch(`${inputs['api-url']}${endpoint}`, {
-    method,
-    headers: {
-      authorization: `token ${inputs['github-token']}`,
-      'content-type': 'application/json',
-    },
-    body: JSON.stringify(body),
-  })
+  try {
+    const response = await fetch(`${inputs['api-url']}${endpoint}`, {
+      method,
+      headers: {
+        authorization: `token ${inputs['github-token']}`,
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    })
 
-  if (response.status !== 200) {
+    return response.json()
+  } catch (error) {
     logWarning(`Please ensure that Github App is installed ${GITHUB_APP_URL}`)
+    logError(error)
   }
-
-  return response.json()
 }
 
 exports.callApi = callApi
