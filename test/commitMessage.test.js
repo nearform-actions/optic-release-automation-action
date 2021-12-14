ca1a73e671d73cafc75c6e1f6682b58d8ae3fd04
Jose Bravo <ja.bravo.isidro@gmail.com>
2021-12-14 09:48:26 +0100

    chore: Update test to not use deprecrated function

--- test/commitMessage.test.js
+++ test/commitMessage.test.js
@@ -4,14 +4,14 @@ const tap = require('tap')
 const transformCommitMessage = require('../utils/commitMessage')
 
 tap.test('Handles normal commit messages', async t => {
-  t.equals(
+  t.equal(
     'Release v5.1.0',
     transformCommitMessage('Release {version}', 'v5.1.0')
   )
 })
 
 tap.test('Handles customized messages', async t => {
-  t.equals(
+  t.equal(
     '[v5.1.0] Released! Some long text goes here!',
     transformCommitMessage(
       '[{version}] Released! Some long text goes here!',
@@ -21,7 +21,7 @@ tap.test('Handles customized messages', async t => {
 })
 
 tap.test('Handles messages with quotes', async t => {
-  t.equals(
+  t.equal(
     '[v5.1.0] \\"Quotes\\"',
     transformCommitMessage('[{version}] "Quotes"', 'v5.1.0')
   )
