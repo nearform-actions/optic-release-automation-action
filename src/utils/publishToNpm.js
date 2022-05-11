3beddd4aa50319b89c4eade929a8f73c427c49f1
Ian Lovell <ian.lovell@nearform.com>
2022-05-11 11:30:02 +0100

    fix: add not published check

--- src/utils/publishToNpm.js
+++ src/utils/publishToNpm.js
@@ -2,6 +2,38 @@
 
 const { runSpawn } = require('./runSpawn')
 
+async function allowNpmPublish(version) {
+  const run = runSpawn()
+
+  // We need to check if the package was already published. This can happen if
+  // the action was already executed before, but it failed in its last step
+  // (GH release).
+  let packageName = null
+  try {
+    const packageInfo = await run('npm', ['view', '--json'])
+    packageName = packageInfo ? JSON.parse(packageInfo).name : null
+  } catch (error) {
+    if (!error?.message?.match(/code E404/)) {
+      throw error
+    }
+  }
+
+  // Package has not been published before
+  if (!packageName) {
+    return true
+  }
+
+  // NPM only looks into the remote registry when we pass an explicit
+  // package name & version, so we don't have to fear that it reads the
+  // info from the "local" package.json file.
+  const packageVersionInfo = await run('npm', [
+    'view',
+    `${packageName}@${version}`,
+  ])
+
+  return packageVersionInfo === ''
+}
+
 async function publishToNpm({
   npmToken,
   opticToken,
@@ -17,18 +49,7 @@ async function publishToNpm({
     `//registry.npmjs.org/:_authToken=${npmToken}`,
   ])
 
-  // We need to check if the package was already published. This can happen if
-  // the action was already executed before, but it failed in its last step
-  // (GH release).
-  const pkgInfo = JSON.parse(await run('npm', ['view', '--json']))
-
-  // NPM only looks into the remote registry when we pass an explicit
-  // package name & version, so we don't have to fear that it reads the
-  // info from the "local" package.json file.
-  const allowNpmPublish =
-    '' === (await run('npm', ['view', `${pkgInfo.name}@${version}`]))
-
-  if (allowNpmPublish) {
+  if (await allowNpmPublish(version)) {
     await run('npm', ['pack', '--dry-run'])
     if (opticToken) {
       const otp = await run('curl', ['-s', `${opticUrl}${opticToken}`])
