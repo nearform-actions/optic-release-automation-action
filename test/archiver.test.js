0fcdb8cfed2ab477c30da3a8b47c9d2694bb9916
Davide Roffo <davide.roffo@nearform.com>
2022-08-01 16:37:13 +0200

    feat: add unit test for adm-zip

--- test/archiver.test.js
+++ test/archiver.test.js
@@ -5,7 +5,7 @@ const tap = require('tap')
 tap.test('throws an error if path not found', async () => {
   const archiverModule = tap.mock('../src/utils/archiver.js', {
     'fs/promises': {
-      lstat: () => ({
+      lstat: async () => ({
         isDirectory: () => {
           throw Error()
         },
@@ -13,5 +13,28 @@ tap.test('throws an error if path not found', async () => {
     },
   })
 
-  tap.rejects(archiverModule.archiveItem())
+  tap.rejects(archiverModule.archiveItem('path', 'out.zip'))
+})
+
+tap.test('does not throw any errors', async () => {
+  const archiverModule = tap.mock('../src/utils/archiver.js', {
+    'fs/promises': {
+      lstat: async () => ({
+        isDirectory: () => true,
+      }),
+    },
+    'adm-zip': function Mocked() {
+      this.addLocalFolderPromise = async function () {
+        return null
+      }
+      this.addLocalFile = function () {
+        return null
+      }
+      this.writeZipPromise = async function () {
+        return null
+      }
+    },
+  })
+
+  tap.resolves(archiverModule.archiveItem('path', 'out.zip'))
 })
