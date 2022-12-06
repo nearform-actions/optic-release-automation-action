dd7bb2d9a429aa3ec26b62e0ef23a5ac91257f7b
Sameer Srivastava <sameer.srivastava@nearform.com>
2022-12-06 20:31:21 +0530

    Fix: Truncate PR body if exceeds max size (#196)
    * chore: added logging
    
    * chore: truncate pr body if exceeds limitation
    
    * chore: added tests
    
    * chore: template updated
    
    * chore: release meta added after truncation
    
    * chore: unnecessary spaces removed
    
    * chore: truncating only release notes
    
    * chore: limit exceeded message changed
    
    * chore: test improvements
    
    * chore: other tests update

--- test/releaseNotes.test.js
+++ test/releaseNotes.test.js
@@ -1,10 +1,16 @@
 'use strict'
 
-const tap = require('tap')
+const { test } = require('tap')
+const _template = require('lodash.template')
+const fs = require('fs')
+const path = require('path')
 
-const { getPrNumbersFromReleaseNotes } = require('../src/utils/releaseNotes')
+const {
+  getPrNumbersFromReleaseNotes,
+  getPRBody,
+} = require('../src/utils/releaseNotes')
 
-tap.test('Should return the correct PR numbers', async () => {
+test('Should return the correct PR numbers', async t => {
   const testReleaseNotes = `
     ## Whats Changed\n +
     * chore 15 by @people in https://github.com/owner/repo/pull/13\n
@@ -34,5 +40,56 @@ tap.test('Should return the correct PR numbers', async () => {
   const result = getPrNumbersFromReleaseNotes(testReleaseNotes)
   const expected = ['13', '15', '16', '18', '42', '50', '52', '53']
 
-  tap.same(result, expected)
+  t.same(result, expected)
+})
+
+test('Should return truncated PR body', async t => {
+  const tpl = fs.readFileSync(path.join(__dirname, '../src/pr.tpl'), 'utf8')
+
+  const testReleaseNotes = `
+    ## Whats Changed\n +
+    * chore 15 by @people in https://github.com/owner/repo/pull/13\n
+    * chore 18 by @people in https://github.com/owner/repo/pull/15\n
+    * chore 19 by @people in https://github.com/owner/repo/pull/16\n
+    * chore 21 by @people in https://github.com/owner/repo/pull/18\n
+    * fix 26 by @people in https://github.com/owner/repo/pull/42\n
+    * feature 30 by @people in https://github.com/owner/repo/pull/50\n
+    * fix 27 by @people in https://github.com/owner/repo/pull/52\n
+    * fix 32 by @people in https://github.com/owner/repo/pull/53\n
+    \n
+    \n
+    ## New Contributors\n
+    * @people made their first contribution in https://github.com/owner/repo/pull/13\n
+    * @people made their first contribution in https://github.com/owner/repo/pull/16\n
+    * @people made their first contribution in https://github.com/owner/repo/pull/42\n
+    * @people made their first contribution in https://github.com/owner/repo/pull/53\n
+    \n
+    \n
+    ## New documentation\n
+    * Link: https://somewhere.com/on/the/internet
+    \n
+    \n
+    **Full Changelog**: https://github.com/owner/repo/compare/v1.0.20...v1.1.0
+`
+
+  let longPrBody = testReleaseNotes
+  for (let i = 0; i < 70; i++) {
+    longPrBody = longPrBody + testReleaseNotes
+  }
+
+  t.ok(longPrBody.length > 60000)
+
+  const truncatedPrBody = getPRBody(_template(tpl), {
+    newVersion: '1.0.0',
+    draftRelease: { id: 1, body: longPrBody },
+    inputs: [],
+    author: 'test',
+    artifact: null,
+  })
+  t.ok(truncatedPrBody.length < 65536)
+  t.ok(
+    truncatedPrBody.includes(
+      `<release-meta>{"id":1,"version":"1.0.0"}</release-meta>`
+    )
+  )
 })
