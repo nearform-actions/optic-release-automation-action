9d1f7cd41cb628eda8cd59c92b22145a892559db
Danijel Maksimovic <danijel.maksimovic@nearform.com>
2022-12-14 14:33:25 +0100

    refactor runSpawn to use @actions/exec

--- src/utils/runSpawn.js
+++ src/utils/runSpawn.js
@@ -1,35 +1,20 @@
 'use strict'
 
-const { spawn } = require('child_process')
+const { getExecOutput } = require('@actions/exec')
 
-function runSpawn({ cwd } = {}) {
-  return (cmd, args) => {
-    return new Promise((resolve, reject) => {
-      const cli = spawn(cmd, args, { cwd, env: process.env, shell: true })
-      cli.stdout.setEncoding('utf8')
-      cli.stderr.setEncoding('utf8')
+async function runSpawn({ cwd } = {}) {
+  return async (cmd, args) => {
+    const { exitCode, stdout, stderr } = await getExecOutput(cmd, args, { cwd })
 
-      let stdout = ''
-      let stderr = ''
-      cli.stdout.on('data', data => {
-        stdout += data
-      })
-      cli.stderr.on('data', data => {
-        stderr += data
-      })
-      cli.on('close', (code, signal) => {
-        if (code === 0) {
-          return resolve(stdout.trim())
-        }
-        reject(
-          new Error(
-            `${cmd} ${args.join(
-              ' '
-            )} returned code ${code} and signal ${signal}\nSTDOUT: ${stdout}\nSTDERR: ${stderr}`
-          )
-        )
-      })
-    })
+    if (exitCode === 0) {
+      return stdout
+    }
+
+    throw new Error(
+      `${cmd} ${args.join(
+        ' '
+      )} returned code ${exitCode} \nSTDOUT: ${stdout}\nSTDERR: ${stderr}`
+    )
   }
 }
 
