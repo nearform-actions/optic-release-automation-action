7366dbf3720b1274052f793d606a6c74a62ce697
Davide Roffo <davide.roffo@nearform.com>
2022-12-20 11:54:17 +0100

    don't update tags refs if prerelease

--- src/release.js
+++ src/release.js
@@ -113,16 +113,13 @@ module.exports = async function ({ github, context, inputs }) {
 
   try {
     const syncVersions = /true/i.test(inputs['sync-semver-tags'])
+    const { major, minor, patch, prerelease } = semver.parse(version)
+    const isPreRelease = prerelease.length > 0
 
-    if (syncVersions) {
-      const { major, minor, patch, prerelease } = semver.parse(version)
-
+    if (syncVersions && !isPreRelease) {
       await tagVersionInGit(`v${major}`)
       await tagVersionInGit(`v${major}.${minor}`)
       await tagVersionInGit(`v${major}.${minor}.${patch}`)
-      if (prerelease.length > 0) {
-        await tagVersionInGit(`v${major}.${minor}.${patch}-${prerelease[0]}`)
-      }
     }
   } catch (err) {
     core.setFailed(`Unable to update the semver tags ${err.message}`)
