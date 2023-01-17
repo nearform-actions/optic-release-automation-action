26ccf2ed60700905dec7c2abf486c6d731b5d2b9
Sameer Srivastava <sameer.srivastava@nearform.com>
2023-01-17 18:35:33 +0530

    chore: no-verify added to commit

--- dist/index.js
+++ dist/index.js
@@ -79368,6 +79368,7 @@ module.exports = async function ({ context, inputs, packageVersion }) {
   await execWithOutput('git', ['add', '-A'])
   await execWithOutput('git', [
     'commit',
+    '--no-verify',
     '-m',
     `"${transformCommitMessage(messageTemplate, newVersion)}"`,
   ])
