5634d5ead1e7da457db8728a3dc8d4843e52b52c
Guilherme Kammsetzer <guilherme@kammsetzer.dev>
2023-02-08 17:58:44 -0300

    test: refactor index/bumpVersion tests
    Remove tests for event type on index. This is now done in action.yml.
    Move bumpVersion tests to it's own file.

--- test/bumpVersion.test.js
+++ test/bumpVersion.test.js
@@ -6,21 +6,19 @@ const sinon = require('sinon')
 
 const actionLog = require('../src/log')
 
-const { PR_TITLE_PREFIX } = require('../src/const')
-
 function buildStubbedAction() {
   const logStub = sinon.stub(actionLog)
-  const releaseStub = sinon.stub()
-  const openPrStub = sinon.stub()
   const bumpStub = sinon.stub()
   const execWithOutputStub = sinon.stub()
+  const releaseStub = sinon.stub()
+  const openPrStub = sinon.stub()
   const coreStub = sinon.stub()
   const conventionalcommitsStub = sinon.stub()
 
-  const { runAction, bumpVersion } = proxyquire('../src/index', {
+  const bumpVersion = proxyquire('../src/bumpVersion', {
     './log': logStub,
-    './release': releaseStub.resolves(),
-    './openPr': openPrStub.resolves(),
+    './release': releaseStub,
+    './openPr': openPrStub,
     './utils/execWithOutput': {
       execWithOutput: () => execWithOutputStub,
     },
@@ -37,7 +35,6 @@ function buildStubbedAction() {
   })
 
   return {
-    action: runAction,
     bumpVersion,
     stubs: {
       logStub,
@@ -54,95 +51,6 @@ function buildStubbedAction() {
 tap.afterEach(() => {
   sinon.restore()
 })
-
-const DEFAULT_ACTION_DATA = {
-  inputs: {},
-  packageVersion: '3.1.1',
-  context: {
-    eventName: 'pull_request',
-    repo: {
-      repo: {},
-      owner: {},
-    },
-    payload: {
-      action: 'closed',
-      pull_request: {
-        user: { login: 'optic-release-automation[bot]' },
-        title: PR_TITLE_PREFIX,
-      },
-    },
-  },
-}
-
-tap.test(
-  'should not run if the event is not workflow_dispatch or pull_request',
-  async () => {
-    const { action, stubs } = buildStubbedAction()
-    await action({
-      ...DEFAULT_ACTION_DATA,
-      context: {
-        ...DEFAULT_ACTION_DATA.context,
-        eventName: 'something_else',
-      },
-    })
-
-    sinon.assert.calledWithExactly(stubs.logStub.logError, 'Unsupported event')
-  }
-)
-
-tap.test(
-  "the release feature should be called if it's a pull request",
-  async () => {
-    const { action, stubs } = buildStubbedAction()
-    await action({
-      ...DEFAULT_ACTION_DATA,
-      context: {
-        ...DEFAULT_ACTION_DATA.context,
-        eventName: 'pull_request',
-      },
-    })
-
-    sinon.assert.notCalled(stubs.logStub.logError)
-    sinon.assert.calledOnce(stubs.releaseStub)
-  }
-)
-
-tap.test(
-  "the bump feature should be called if it's a workflow_dispatch",
-  async () => {
-    const { action, stubs } = buildStubbedAction()
-    await action({
-      ...DEFAULT_ACTION_DATA,
-      context: {
-        ...DEFAULT_ACTION_DATA.context,
-        eventName: 'workflow_dispatch',
-      },
-    })
-
-    sinon.assert.notCalled(stubs.logStub.logError)
-    sinon.assert.calledOnce(stubs.openPrStub)
-  }
-)
-
-tap.test(
-  'npm should be configured if called with npm-token in inputs',
-  async () => {
-    const { action, stubs } = buildStubbedAction()
-    await action({
-      ...DEFAULT_ACTION_DATA,
-      inputs: {
-        'npm-token': 'a-token',
-      },
-      context: {
-        ...DEFAULT_ACTION_DATA.context,
-        eventName: 'workflow_dispatch',
-      },
-    })
-
-    sinon.assert.notCalled(stubs.logStub.logError)
-    sinon.assert.calledOnce(stubs.openPrStub)
-  }
-)
 
 tap.test(
   'semver-auto: should call getAutoBumpedVersion if semver is auto',
