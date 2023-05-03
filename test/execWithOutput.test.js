c85dbdf93e5f355a9cec1394218534ae25241b1c
Alan Slater <alan.slater@nearform.com>
2023-05-03 17:33:57 +0100

    Add unit test

--- test/execWithOutput.test.js
+++ test/execWithOutput.test.js
@@ -71,3 +71,35 @@ tap.test('rejects if exit code is not 0', async t => {
   t.rejects(execWithOutputModule.execWithOutput('command'))
   execStub.calledWith('command')
 })
+
+tap.only('passes env vars excluding `INPUT_*` env vars', async t => {
+  const INPUT_NPM_TOKEN = 'some-secret-value'
+  const INPUT_OPTIC_TOKEN = 'another-secret-value'
+  const ACTIONS_ID_TOKEN_REQUEST_URL = 'https://example.com'
+  const GITHUB_EVENT_NAME = 'someEvent'
+
+  sinon.stub(process, 'env').value({
+    ...process.env,
+    INPUT_NPM_TOKEN,
+    INPUT_OPTIC_TOKEN,
+    ACTIONS_ID_TOKEN_REQUEST_URL,
+    GITHUB_EVENT_NAME,
+  })
+
+  execStub.resolves(0)
+  execWithOutputModule.execWithOutput('command', [])
+
+  const envInExec = execStub.firstCall.lastArg.env
+
+  // Check custom env vars are preserved
+  t.has(envInExec, { ACTIONS_ID_TOKEN_REQUEST_URL })
+  t.has(envInExec, { GITHUB_EVENT_NAME })
+
+  // Check INPUT_* env vars are removed
+  t.notHas(envInExec, { INPUT_NPM_TOKEN })
+  t.notHas(envInExec, { INPUT_OPTIC_TOKEN })
+
+  // Check "real" env vars are preserved.
+  // Its value will vary by test runner, so just check it is present.
+  t.hasProp(envInExec, 'NODE')
+})
