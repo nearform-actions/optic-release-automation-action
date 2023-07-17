d7dae99dc2c392d586756e34afa978c00818ebf2
Simone Busoli <simone.busoli@nearform.com>
2023-07-17 14:11:26 +0200

    chore: ensure trailing slash in api-url input (#337)
    Fixes #330

--- test/callApi.test.js
+++ test/callApi.test.js
@@ -17,7 +17,7 @@ const setup = status => {
       },
     }),
   })
-  return { logStub, callApiProxy }
+  return { logStub, callApiProxy, fetchStub }
 }
 
 tap.afterEach(() => {
@@ -32,7 +32,9 @@ tap.test('Call api warns if code is not 200', async () => {
       method: 'PATCH',
       body: {},
     },
-    {}
+    {
+      'api-url': 'whatever',
+    }
   )
   sinon.assert.calledOnce(logStub.logWarning)
 })
@@ -45,7 +47,33 @@ tap.test('Call api does not warn if code is  200', async () => {
       method: 'PATCH',
       body: {},
     },
-    {}
+    { 'api-url': 'whatever' }
   )
   sinon.assert.notCalled(logStub.logWarning)
+})
+
+tap.test('Call api does not append slash to api url if present', async () => {
+  const { fetchStub, callApiProxy } = setup(200)
+  await callApiProxy.callApi(
+    {
+      endpoint: 'release',
+      method: 'PATCH',
+      body: {},
+    },
+    { 'api-url': 'whatever/' }
+  )
+  sinon.assert.calledWith(fetchStub, 'whatever/release')
+})
+
+tap.test('Call api appends slash to api url if not present', async () => {
+  const { fetchStub, callApiProxy } = setup(200)
+  await callApiProxy.callApi(
+    {
+      endpoint: 'release',
+      method: 'PATCH',
+      body: {},
+    },
+    { 'api-url': 'whatever' }
+  )
+  sinon.assert.calledWith(fetchStub, 'whatever/release')
 })
