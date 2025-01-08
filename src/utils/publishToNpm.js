78c97fe052d9ade39dfaa4f9b78c84a6d4c40aa9
Simone Busoli <simone.busoli@gmail.com>
2025-01-08 11:28:52 +0100

    chore: alternative otp provisioning pseudo code

--- src/utils/publishToNpm.js
+++ src/utils/publishToNpm.js
@@ -63,18 +63,57 @@ async function publishToNpm({
   if (await allowNpmPublish(version)) {
     await execWithOutput('npm', ['pack', '--dry-run'])
 
-    if (opticToken) {
-      const packageInfo = await getLocalInfo()
-      const otp = await execWithOutput('curl', [
-        '-s',
-        '-d',
-        JSON.stringify({ packageInfo: { version, name: packageInfo?.name } }),
-        '-H',
-        'Content-Type: application/json',
-        '-X',
-        'POST',
-        `${opticUrl}${opticToken}`,
-      ])
+    let otp
+
+    if (opticToken || ngrokApiKey) {
+      if (opticToken) {
+        const packageInfo = await getLocalInfo()
+        
+        // push notification via optic mobile app
+        otp = await execWithOutput('curl', [
+          '-s',
+          '-d',
+          JSON.stringify({ packageInfo: { version, name: packageInfo?.name } }),
+          '-H',
+          'Content-Type: application/json',
+          '-X',
+          'POST',
+          `${opticUrl}${opticToken}`,
+        ])
+      } else if (ngrokApiKey) {
+        // 1. run a fastify app which exposes a simple web UI with a text input to get the OTP
+        // 2. expose the app via ngrok
+        // 3. print the url to the app on the workflow execution output
+        // 4. wait for some time or a timeout
+        // 5. if we get the token from the user, continue with the execution, otherwise timeout
+        const app = fastify()
+
+        app.get('/', () => {
+          // serve static page with text input which, when submitted, POSTs the OTP to /otp url
+        })
+
+        let otpPromiseResolve
+        const otpPromise = new Promise(resolve => { otpPromiseResolve = resolve })
+
+        app.post('/otp', async req => {
+          const otp = req.body.otp
+
+          otpPromiseResolve(otp)
+        })
+
+        app.listen({ host: ..., port: ...})
+
+        // expose the fastify app via nkgrok and get the ngrok app url, see https://github.com/ngrok/ngrok-javascript
+        const ngrokUrl = ...
+        
+        console.log("Please visit this url to provide the OTP:", ngrokUrl)
+
+        // timeout mechanism here
+        otp = await otpPromiseResolve
+
+        // tear down fastify application here (optional, once the action finishes executing it will tear down automatically)
+      }
+      
       await execWithOutput('npm', ['publish', '--otp', otp, ...flags])
     } else {
       await execWithOutput('npm', ['publish', ...flags])
